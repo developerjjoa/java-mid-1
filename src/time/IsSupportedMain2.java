e6efd8f7b3555f9d72681080f650acded7c588b4
developerjjoa <developerjjoa@gmail.com>
2025-08-05 22:29:58 +0900

    isSupported()로 현재 타입에서 특정 시간 단위나 필드를 사용할 수 있는지 확인하는 예제

---
+++ src/time/IsSupportedMain2.java
@@ -0,0 +1,18 @@
+package time;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoField;
+
+public class IsSupportedMain2 {
+    public static void main(String[] args) {
+        LocalDate now = LocalDate.now();
+
+        boolean supported = now.isSupported(ChronoField.SECOND_OF_MINUTE);
+        System.out.println("supported = " + supported);
+
+        if (supported) {
+            int second = now.get(ChronoField.SECOND_OF_MINUTE);
+            System.out.println("second = " + second);
+        }
+    }
+}
