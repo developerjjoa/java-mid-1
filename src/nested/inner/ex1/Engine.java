7b3975c20b9f7828790bbe074e0bfc11bedfa095
developerjjoa <developerjjoa@gmail.com>
2025-08-06 23:09:08 +0900

    내부 클래스 활용 리팩토링 전 - Car에서만 사용하는 Engine 클래스, 엔진을 시작하기 위해서는 차의 충전 레벨과 차량 이름 필요, Car 인스턴스 참조를 생성자에 보관, car.getChargeLevel(), car.getModel() 필요

---
+++ src/nested/inner/ex1/Engine.java
@@ -0,0 +1,16 @@
+package nested.inner.ex1;
+
+public class Engine {
+    //Car 에서만 사용
+
+    private Car car;
+
+    public Engine(Car car) {
+        this.car = car;
+    }
+
+    public void start() {
+        System.out.println("충전 레벨 확인: " + car.getChargeLevel());
+        System.out.println(car.getModel() + "의 엔진을 구동합니다.");
+    }
+}
